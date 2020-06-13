4d20018d1e81f5828f4e76a966c7bbdd7fe6a941
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-13 17:19:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,19 @@
 # Add your variables here
-def calculate(num1, num2) 
-    
-    first_number = num1
-    second_number = num2
 
 
-    sum = first_number + second_number
-    return sum
+first_number = num1
+second_number = num2
 
-    difference = first_number - second_number
-    return difference
 
-    product = first_number * second_number
-    return product
-    
-    quotient = first_number / second_number
-    return quotient
+sum = first_number + second_number
+return sum
 
-end
+difference = first_number - second_number
+return difference
+
+product = first_number * second_number
+return product
+
+quotient = first_number / second_number
+return quotient
+
